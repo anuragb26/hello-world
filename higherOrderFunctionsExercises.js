47850ede1d4b7702aa2cce17f5c13fa89aa9808a
Anurag Bajaj <Anurag Bajaj>
2016-04-06 22:07:01 +0530

    motherChild_1

--- higherOrderFunctionsExercises.js
+++ higherOrderFunctionsExercises.js
@@ -7,3 +7,12 @@ var createSingleArray = function(array1,array2)
 var arrays = [[1, 2, 3], [4, 5], [6]];
 
 console.log(arrays.reduce(createSingleArray));
+
+
+var ancestors = JSON.parse(ANCESTRY_FILE);
+
+console.log(ancestors.length);
+
+
+
+
