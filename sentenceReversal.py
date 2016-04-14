afb8b446633438a8f73cb758d089ceccdfa74292
Anurag Bajaj <abajaj@operamediaworks.com>
2016-04-14 19:54:32 +0530

    algo5

--- sentenceReversal.py
+++ sentenceReversal.py
@@ -1,22 +1,45 @@
+def reverseWord(word,start,end):
+    reverseWord=""
+    while(end>=start):
+        reverseWord=reverseWord + word[end]
+        end-=1
+ #   print("in reverse word ",reverseWord)    
+    return reverseWord
+    
 
-def reverseSentence(s):
-	l=len(s)
-	i=0
-	s ="".join(reversed(s))
-	wordStart=0
-	wordEnd=0
-	while i < len:
-		if(s[i]== ''):
-			reverseWord()
-			wordStart=i+1
-			worEnd=i+1
-		else:
-			worEnd++
+    
+
+def reverseSentence(sent):
+    wordStart=-1
+    wordEnd=-1
+    reversedSentence=""
+    length =len(sent)-1
+    index=0
+    while(index <=length):
+       # print ("index is ",index, " and character is ",sent[index]," and wordstart and wordEnd ",wordStart," -> ",wordEnd)
+        if(sent[index]==" " and wordEnd==-1 and wordStart==-1):
+        #    print ("coming in first condition ",sent[index])
+            reversedSentence=reversedSentence+sent[index]
+        elif((sent[index]==" " or index==length) and wordEnd == -1):
+           # print ("coming in second condition ",sent[index]," wordStart ",wordStart," wordEnd ",wordEnd)
+            if(index == length):
+                wordEnd=index
+            else:
+                wordEnd=index-1
+            reversedSentence = reversedSentence + str(reverseWord(sent,wordStart,wordEnd)) + " "
+            wordEnd=-1
+            wordStart=-1
+        elif(sent[index]!= " " and wordStart ==-1):
+           #  print ("coming in third condition ",sent[index])
+             wordStart=index
+        index+=1
+       
+    return reverseWord(reversedSentence,0,length)
+
+print(reverseSentence("abc def   ghi"))
 
 
-	return s
 
-print reverseSentence("anurag bajaj")
 
 
 
