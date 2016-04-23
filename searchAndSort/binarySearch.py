2eb06333ffde742ae9f9b2d7bebcd98884ea7723
Anurag Bajaj <Anurag Bajaj>
2016-04-23 14:18:02 +0530

    binarySearch

--- searchAndSort/binarySearch.py
+++ searchAndSort/binarySearch.py
@@ -12,8 +12,27 @@ def binarySearch(testlist,item):
 		elif(item < testlist[mid]):
 			end=mid-1
 	return found
+
+
+def recursiveBinarySearch(testList,start,end,item):
+	if(start>end):
+		return False
+	else:	
+		mid=(start+end)//2
+		if(testlist[mid]==item):
+			return True
+		elif(item < testList[mid]):
+			return recursiveBinarySearch(testList,start,mid-1,item)
+		elif(item > testList[mid]):
+			return recursiveBinarySearch(testList,mid+1,end,item)	
  
-print("Something works")
 testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42]
 print("bs " + str(binarySearch(testlist, 3)))
 print("bs " + str(binarySearch(testlist, 13)))
+
+print("bs " + str(recursiveBinarySearch(testlist,0,len(testlist)-1,3)))
+print("bs " + str(recursiveBinarySearch(testlist, 0,len(testlist)-1,32)))
+
+
+
+
