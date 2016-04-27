49ba700a59b91d1046f0af7cbdec765a57e1bb24
Anurag Bajaj <Anurag Bajaj>
2016-04-28 00:33:20 +0530

    Working with lists

---
+++ HeadFirstPython/lists.py
@@ -0,0 +1,21 @@
+
+cast=["Cleese","Palin","Jones","Idle"]
+
+cast.extend(["Gilliam","Chapman"])
+
+cast.remove("Chapman")
+
+cast.insert(0,"Chapman")
+
+print(cast)
+
+
+movies = ["The Holy Grail","The Life of Brian","The Meaning of Life"]
+
+print(movies)
+
+movies.insert(1,1975)
+movies.insert(3,1979)
+movies.append(1983)
+
+print(movies)
