b259d33588498892a81ec790f4949becf7e016de
Anurag Bajaj <Anurag Bajaj>
2016-04-28 21:33:57 +0530

    recursiveList

--- HeadFirstPython/lists.py
+++ HeadFirstPython/lists.py
@@ -7,15 +7,50 @@ cast.remove("Chapman")
 
 cast.insert(0,"Chapman")
 
-print(cast)
+#print(cast)
 
 
 movies = ["The Holy Grail","The Life of Brian","The Meaning of Life"]
 
-print(movies)
+#print(movies)
 
 movies.insert(1,1975)
 movies.insert(3,1979)
 movies.append(1983)
 
-print(movies)
+#print(movies)
+
+movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
+["Graham Chapman", ["Michael Palin", "John Cleese",
+"Terry Gilliam", "Eric Idle", "Terry Jones"]]]
+
+#print("Nested lists")
+#print(movies)
+
+
+#for each_item in movies:
+	#if(isinstance(each_item,list)):
+	#	for nested_item in each_item:
+	#		print(nested_item)
+	#else:
+		#print(each_item)
+
+
+
+def recursivePrintLists(entity):
+	if(not isinstance(entity,list)):
+		print(entity)
+	else:
+		for each_item in entity:
+			recursivePrintLists(each_item)
+
+
+
+
+recursivePrintLists(movies)
+
+
+
+
+
+
