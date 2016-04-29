96a8730a6dd79c9c3401951db4bd8caf1c1d3052
Anurag Bajaj <abajaj@operamediaworks.com>
2016-04-29 15:55:18 +0530

    Chapter2_b

---
+++ HeadFirstPython/nester/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name = 'nester',
+		version = '1.0.0',
+		py_modules = ['nester'],
+		author  = 'anurag bajaj',
+		author_email = 'anuragb26@gmail.com',
+		url = 'http://www.anuragb26.com',
+		description = 'Printer of nester lists'
+)
---
+++ HeadFirstPython/testingNester.py
@@ -0,0 +1,9 @@
+import nester
+
+
+movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
+["Graham Chapman", ["Michael Palin", "John Cleese",
+"Terry Gilliam", "Eric Idle", "Terry Jones"]]]
+
+
+nester.printRecursiveList(movies)
