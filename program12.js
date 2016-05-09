d7d09ed7995b5d242e8df2def7bb585ec91929f3
Anurag Bajaj <abajaj@operamediaworks.com>
2016-05-09 18:48:39 +0530

    pipe

---
+++ program12.js
@@ -0,0 +1,30 @@
+var http=require('http');
+var mapStream= require('through2-map');
+
+
+var mapStreamLogic = function (data)
+{
+    return data.toString().toUpperCase();
+}
+var server = http.createServer(function (request,response)
+{
+    
+    if(request.method == 'POST')
+        {
+            request.pipe(mapStream(function(data)
+            {
+                return data.toString().toUpperCase()
+                
+            })).pipe(response);
+            
+        }
+    
+    /*
+    if(request.method == 'POST')
+        {
+            request.pipe(mapStream(mapStreamLogic(data))).toUpperCase();
+        }
+    */
+});
+
+server.listen(Number(process.argv[2]));
