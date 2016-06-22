44ce09730c410a56b4888c1c12b6eabe66c4dc7d
Anurag Bajaj <Anurag Bajaj>
2016-06-23 00:44:04 +0530

    MySqlConnection

--- test.js
+++ test.js
@@ -1,6 +1,51 @@
 var _ = require('underscore');
+var mysql = require("mysql");
 
 _.each([1,2,3],function(arrayElement)
 {
 	console.log(arrayElement*2);
 });
+
+var con = mysql.createConnection
+	({
+		host: "127.0.0.1",
+		user:"root",
+		password:"",
+		database:"vt_service"
+	});
+
+con.connect(function(err)
+	{
+		if(err)
+		{
+			console.log("Error connecting to Db");
+			return ;
+		}
+		console.log("Connection Established");
+	});
+
+//var queryString = "SELECT `id`, `name`, `image`, `description`, `branding`, `rating`, `setup_fee`, `transaction_fees`, `how_to_url`, `currencies` FROM `vt_services`";
+var queryString = "SELECT * from  `vt_services`";
+
+con.query(queryString,
+		function(err,rows)
+{
+	if(err)
+	{
+		console.log("received error");
+	}
+		console.log(rows);
+
+})
+
+
+
+
+
+
+con.end(function(err)
+	{
+
+	})
+
+
