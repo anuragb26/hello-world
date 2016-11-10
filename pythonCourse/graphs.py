5be97ae90b1e5ce6f14e1b8537a7316843979fbf
Anurag Bajaj <abajaj@operamediaworks.com>
2016-11-10 11:26:42 +0530

    graphs2

--- pythonCourse/graphs.py
+++ pythonCourse/graphs.py
@@ -14,3 +14,49 @@ class Node(obj):
 		return self.name
 	def __str__(self):
 		return self.name
+
+class Edge(obj):
+
+	def __init__(self,src,dest):
+		if(not isinstance(src,Node)):
+			raise ValueError("Please provide an instance of Node Class for source")
+		if(not isinstance(dest,Node)):
+			raise ValueError("Please provide an instance of Node Class for destination")
+		self.src=src
+		self.dest=dest
+
+	def getSource(self):
+		return self.src
+
+	def getDestination(self):
+		return self.dest
+
+	def __str__(self):
+		return self.src.getName() + "-->" self.dest.getName()
+
+
+
+class Digraph():
+
+	def __init__(self):
+		self.edges={}
+
+	def addNode(self,node):
+		if not isinstance(node,Node):
+			raise TypeError("Please provide an instance of Node class to add in the Graph")
+		if node in self.edges:
+			raise ValueError("Node already exists in the Graph")
+		self.edges[node]=[]
+
+	def addEdge(self,src,dest):
+	def connectedTo(self,node):
+		return self.edges[node]
+
+	def hasNode(self,node):
+		return node in self.edges
+
+	def getNode(self,name):
+		for node in self.edges:
+			if name == self.edges[node].getName():
+				return node
+		raise NameError("No such Node Exists") 
